d8de2a6a18b95c51f9ab4c02d35af9118a89ed48
Jose Arellano <157650048+sway210@users.noreply.github.com>
2025-04-01 16:49:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Pet.h"
+#include "Dog.h"
+
 
 int main() {
     Pet pet1("Rex", "Dog", 3, 6);
@@ -13,9 +15,25 @@ int main() {
 
     std::cout << "\nComparing pets by name: ";
     int result = pet1.compareTo(pet2);
-    if (result < 0) std::cout << pet1.getName() << " comes before " << pet2.getName() << "\n";
+    if (result < 0) 
+        std::cout << pet1.getName() << " comes before " << pet2.getName() << "\n";
     else if (result > 0) std::cout << pet1.getName() << " comes after " << pet2.getName() << "\n";
     else std::cout << "Both pets have the same name.\n";
+
+    pet2.makeSound();
+    pet1.makeSound();
+
+    //testing the dog object
+    Dog dog1("Buddy", "Dog", 5, 3, "Golden Retriever");
+    std::cout << "\nDog Info:\n";
+    dog1.printInfo();
+    dog1.makeSound();
+
+    //polymorphism
+    Pet* petPtr = new Dog("Fluffy", "Dog", 12, 9, "poodle");
+    petPtr->printInfo();
+    petPtr->makeSound();
+    delete petPtr; // Clean up the dynamically allocated Dog object
 
     return 0;
 }
